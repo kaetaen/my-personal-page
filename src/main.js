1e332feb2cf21b60a4964736a3407f031cc4eea0
kaetaen <kaetaen@tutanota.com>
2020-09-07 14:05:57 -0300

    Add carousel effect

--- src/main.js
+++ src/main.js
@@ -27,9 +27,28 @@ function typewriterEffect (elem) {
   })
 }
 
+function carousel() {
+  const time = 3000, images = document.querySelectorAll('.slider .img')
+  let currentImageIndex = 0, max = images.length;
+  function nextImage () {
+    images[currentImageIndex].classList.remove('selected')
+    
+    currentImageIndex++
+    
+    if (currentImageIndex >= max)
+      currentImageIndex = 0
+
+    images[currentImageIndex].classList.add('selected')
+  }
+
+  setInterval(nextImage, time)
+}
+
+window.addEventListener('load', carousel)
 window.addEventListener('load', menuResponsiveBehavior)
 window.addEventListener('load', () => {
   typewriterEffect('#initial-cover p')
   typewriterEffect('#about-me p')
+  typewriterEffect('#projects p')
 })
 
